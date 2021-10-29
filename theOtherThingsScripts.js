0955f974e39f3bfaeffc1669425ada5677310c93
pachonga9 <pachonga9@gmail.com>
2021-10-29 17:05:50 -0500

    new things

---
+++ theOtherThingsScripts.js
@@ -0,0 +1,24 @@
+console.log("hello world. The script is running.");
+
+var introSound = new Audio("assets/intro.mp3");
+let displayArea = document.getElementById("displayArea");
+let firstText = document.getElementById("firstText");
+let secondText = document.getElementById("secondText");
+let theOtherThings = document.getElementById("theOtherThings");
+let thirdText = document.getElementById("thirdText");
+let fourthText = document.getElementById("fourthText");
+let fifthText = document.getElementById("fifthText");
+// let hideThese = document.querySelectorAll("hidden");
+
+let myButton = (document.getElementById("myButton").onclick = function () {
+  introSound.play();
+  firstText.classList.toggle("fade");
+  secondText.classList.toggle("fade");
+  theOtherThings.classList.toggle("fade");
+  thirdText.classList.toggle("fade");
+  fourthText.classList.toggle("fade");
+  fifthText.classList.toggle("fade");
+  //   setTimeout(function () {
+  //     hideThese.classList.toggle("hide");
+  //   }, 3000);
+});
